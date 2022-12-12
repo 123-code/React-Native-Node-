ddf5ba6e0b64b3ae87d1efec842611f7e5baaf46
123-code <naranjojose256@gmail.com>
2022-12-12 11:48:34 -0500

    File Updates

--- Server/src/index.js
+++ Server/src/index.js
@@ -2,13 +2,21 @@ import express from 'express';
 import mongoose from 'mongoose';
 import  {routes}  from  '../API_Routes';
 import cors from 'cors';
+import http from 'http';
+import https from 'https';
+import fs from 'fs';
 
 const main = async()=>{
     const port = 8000;
     const url = "mongodb://127.0.0.1:27017/catalogo";
+    const privateKey = fs.readFileSync('sslcert/server.key','utf8');
+    const certificate = fs.readFileSync('sslcert/server.cert','utf8');
+    const credentials = {key:privateKey,cert:certificate};
     const server = express();
+    
     server.use(express.json());
     server.use(cors());
+
    // server.use('/api', Routes);
 
 
