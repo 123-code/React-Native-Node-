f0ca9a5963e21a55934ab495567ebaa8932c4830
123-code <naranjojose256@gmail.com>
2023-01-24 16:01:52 -0500

    login changes

--- App/Components/Screens/Login.js
+++ App/Components/Screens/Login.js
@@ -1,61 +1,100 @@
 import {StatusBar} from 'expo-status-bar';
 import React,{useState,useEffect} from 'react';
-import {View,Text,StyleSheet,TextInput,Dimensions,Button,Pressable} from 'react-native';
+import {View,Text,StyleSheet,TextInput,Dimensions,Button,Pressable,Image} from 'react-native';
 import * as Google from 'expo-auth-session/providers/google';
 import axios from 'axios';
 import PostScreen from './PostScreen';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 //import bcrypt from 'bcrypt';
 
 
   export default function LoginScreen({navigation}){
     const [Token,setAccesstoken] = useState();
-    const [userInfo,setUserInfo] = useState('');
+    const [userInfo,setUserInfo] = useState([]);
+    const [Auth,setAuth] = useState();
     const [req,res,prompt] = Google.useAuthRequest({
       androidClientId:'133427487604-9b9b5usg2hh8rl9iu2gs0adel4p80emv.apps.googleusercontent.com',
       iosClientId:'133427487604-fckojm1va3kssread96bbj03grb2m953.apps.googleusercontent.com',
       expoClientId:'133427487604-4m0i8svjekh6iqqb90sq42a5vagp9ec7.apps.googleusercontent.com',
+    },{
+      accessTokenExpiry: 3600,
+      "refresh_token":"tGzv3JOkF0XG5Qx2TlKWIA"
     });
 
     const LoginClient = axios.create({
       baseURL: 'http://172.31.170.131:8000/auth/google/createuser'
     });
 
+ 
     useEffect(()=>{
       if(res?.type === 'success'){
-        setAccesstoken(res.authentication.accessToken);
+
+        setAuth(res);
+        const PersistToken = async()=>{
+          await AsyncStorage.setItem("auth",JSON.stringify(res));
+        };
+        PersistToken();
       }
     },[res])
-//
-    const GetUserData = async()=>{
-      let userData = await fetch('https://openidconnect.googleapis.com/v1/userinfo',{
-        headers:{
-          Authorization:`Bearer ${Token}`
-        }
-      })
-      let userinfo = await userData.json();
-      setUserInfo(userinfo);
-      console.log(userInfo)
+
+    useEffect(()=>{
+      const getUserToken = async()=>{
+const JsonValue = await AsyncStorage.getItem("auth")
+if (JsonValue != null){
+  setAuth(JSON.parse(JsonValue));
+  console.log(Auth)
+}
+      }
+      getUserToken()
+    },[])
+
+
+   const GetuserData = async()=>{
+    let UserInfoResponse =   await fetch('https://www.googleapis.com/oauth2/v2/userinfo',
+    {
+      headers:{Authorization:`Bearer ${Auth.accessToken}`}
+    })
+    UserInfoResponse.json().then(data=>{
+      console.log("Data",data)
+      setUserInfo(data)
+    })
+   }
+     
+ 
+// 
+
+const ShowUserData = ()=>{
+  if(userInfo){
+    return(
+      <View>  
+        <Image source={{uri:userInfo.picture}}></Image>
+        <Text> Bienvenido {userInfo.name} </Text>
+        <Text> {userInfo.email} </Text>
+      </View>
+    )
+  }
+}
+
+    const PostUserData = async()=>{
       try{
-        await GetUserData()
         await axios.post(LoginClient,{
           email:userInfo.email,
           name:userInfo.name,
           imageUrl:userInfo.picture,
           googleId:userInfo.sub,
         })
-        navigation.navigate('Posts')
       }catch(err){
         console.error(err)
       }
     }
-// when authenticated function runs 
  
   return(
     <View style={styles.container}>
+      {ShowUserData()}
       <Text style={styles.text1}> Bienvenido a Payz </Text>
 <Pressable style={styles.button}  
-onPress = {Token ?  GetUserData  : ()=> prompt({useProxy:true,showInRecents:true})}>  
-<Text style={styles.buttonText}> {Token ? " ": "Iniciar Sesión con Google"}  </Text>
+onPress = {Auth ?  GetuserData  : ()=> prompt({useProxy:true,showInRecents:true})}>  
+<Text style={styles.buttonText}> {Auth ? " ": "Iniciar Sesión con Google"}  </Text>
 </Pressable>
 </View>
   ) 
@@ -89,6 +128,10 @@ const styles = StyleSheet.create({
   letterSpacing: 0.25,
   color: 'green',
 },
+Picture:{
+  width:50,
+  height:50
+}
 
 });
 
