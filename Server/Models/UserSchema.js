726d94f3afd2ee093b057b305f179d41b293724f
Jose Ignacio Naranjo <52728126+123-code@users.noreply.github.com>
2023-02-03 15:17:45 +0000

    cambios

--- Server/Models/UserSchema.js
+++ Server/Models/UserSchema.js
@@ -3,7 +3,7 @@ import mongoose, { Schema } from 'mongoose';
 
 const USchema  = new Schema({
     nombre:{required:true,type:String},
-    email:{required:true,type:email},
+    email:{required:true,type:String},
     password:{required:true,type:String},
 })
 
